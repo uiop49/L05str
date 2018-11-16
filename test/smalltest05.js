bb68a7f2fc79aa97efe2ca240203345ff0de7661
Alexander Pavlov <7911562+av-pavlov@users.noreply.github.com>
2018-11-16 16:27:06 +0900

    5 слабых тестов, + болванки для ваших

---
+++ test/smalltest05.js
@@ -0,0 +1,22 @@
+"use strict"                       
+
+const assert = require('assert');
+const fs = require('fs');
+
+describe('ЗАДАЧА 5: функция isDelResult', function(){
+	it('файл L05-05.js существует ', function(){
+		assert(fs.existsSync('L05-05.js'), "Файл L05-05.js не существует");
+	});
+
+	it('файл непуст', function(){
+		var stats = fs.statSync('L05-05.js');
+		var fileSize = stats["size"];
+		assert(fileSize > 24, "Файл пуст или слишком маленький");
+	});
+
+	it('дает истину для пары ALABAMA, ALMA', function(){
+		const isDelResult = require('../L05-05.js');
+		assertStrictEqual(isDelResult("ALABAMA", "ALMA"), true, "неверно для 'ALABAMA', 'ALMA'");
+	});
+
+});
