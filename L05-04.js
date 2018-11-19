a7e496b40fb054598d996027847fd291ffcf29e3
uufswe21 <sdfauren@gmail.com>
2018-11-19 20:16:40 +0900

    -task04 -test04

--- L05-04.js
+++ L05-04.js
@@ -1,1 +1,20 @@
- 
+function isPalindrome(s) {
+	var q = '';
+	var	palindromeFactorCount = 0;
+	
+	for (i = 0; i < s.length; i++) {
+		if (s[i] != ' ') 
+			q += s[i];
+	}
+	
+	for (i = 0; i < Math.round(q.length/2); i++) {
+		if (q[i]==q[q.length-i-1]) palindromeFactorCount++;
+	}
+	
+	if (palindromeFactorCount == Math.round(q.length/2))
+		return true;
+	else
+		return false;
+}
+
+module.exports = isPalindrome;
--- test/test04.js
+++ test/test04.js
@@ -1,1 +1,35 @@
+"use strict"                       
+
+const assert = require('assert');
+const fs = require('fs');
+
+describe('ЗАДАЧА 4: функция isPalindrome', function(){
+	it('файл L05-04.js существует ', function(){
+		assert(fs.existsSync('L05-04.js'), "Файл L05-04.js не существует");
+	});
+
+	it('файл непуст', function(){
+		var stats = fs.statSync('L05-04.js');
+		var fileSize = stats["size"];
+		assert(fileSize > 24, "Файл пуст или слишком маленький");
+	});
+	
+	const isPalindrome = require('../L05-04.js');
+
+	it('дает истину для одного палиндрома', function(){
+		assert.strictEqual(isPalindrome("MADAM I M ADAM"), true, "неверно для 'MADAM I M ADAM'");
+	});
+	
+	it('дает истину для одного палиндром с несколькими подряд идущими пробелами', function(){
+		assert.strictEqual(isPalindrome("MADAM I M    ADAM"), true, "неверно для 'MADAM I M    ADAM'");
+	});
+
+	it('дает ложь для одного непалиндрома', function(){
+		assert.strictEqual(isPalindrome("MADA"), false, "неверно для 'MADA'");
+	});
+	
+	it('дает ложь для одного непалиндрома с несколькими подряд идущими пробелами', function(){
+		assert.strictEqual(isPalindrome("M A  D A MADA"), false, "неверно для 'M A  D A MADA'");
+	});
  
+}); 
