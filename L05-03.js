ae5e82de2b753487e90c79155e5fce3b8d51cb9a
uufswe21 <sdfauren@gmail.com>
2018-11-19 19:38:42 +0900

    -task03 -test03

--- L05-03.js
+++ L05-03.js
@@ -1,1 +1,19 @@
- 
+function longestWordLen(s) {
+var maxCount = 0;
+	symbolCount = 0;	
+	for (i = 0; i < s.length; i++) {
+		if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+			symbolCount++;
+		else {
+			if (maxCount < symbolCount) 
+				maxCount = symbolCount;
+			symbolCount = 0;
+		}
+	}
+if (maxCount != 0)
+	return maxCount;
+else
+	return symbolCount;
+} 
+
+module.exports = longestWordLen;
--- test/test03.js
+++ test/test03.js
@@ -1,1 +1,37 @@
+"use strict"                       
+
+const assert = require('assert');
+const fs = require('fs');
+
+describe('ЗАДАЧА 3: функция longestWordLen', function(){
+	it('файл L05-03.js существует ', function(){
+		assert(fs.existsSync('L05-03.js'), "Файл L05-03.js не существует");
+	});
+
+	it('файл непуст', function(){
+		var stats = fs.statSync('L05-03.js');
+		var fileSize = stats["size"];
+		assert(fileSize > 24, "Файл пуст или слишком маленький");
+	});
+
+	const longestWordLen = require('../L05-03.js');
+	
+	it('Правильно находит длину в пустой строке', function(){	
+		assert.strictEqual(longestWordLen(""), 0, "неверно для слова 'Quick'");
+	});
+	
+	it('Правильно находит длину 1 слова', function(){	
+		assert.strictEqual(longestWordLen("Quickier"), 8, "неверно для слова 'Quick'");
+	});
+	
+	it('Правильно находит длину в дефолтном предложении', function(){	
+		assert.strictEqual(longestWordLen("Quick brown fox jumps, and is gone."), 5, "неверно для слова 'Quick brown fox jumps, and is gone.'");
+	});
+	
+	it('Правильно находит длину в предложении, содержащем другие знаки', function(){	
+		assert.strictEqual(longestWordLen("Quick; brown:> fox-+ jumps^%, and;', is gone."), 5, "неверно для слова 'Quick'");
+	});
+	
+	
+});
  
