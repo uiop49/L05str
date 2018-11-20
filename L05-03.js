4ff592162f6bdf7457b6da853763589aa2c0d4f8
sem <spacersemenov2000@gmail.com>
2018-11-20 11:20:36 +0900

    zad3

--- L05-03.js
+++ L05-03.js
@@ -2,12 +2,14 @@ function longestWordLen(s){
 	var l = s.length ;
 	var q = 0 ;
 	var k = 0 ;
+	var c = 0 ;
 	for(i=0;i<l;i++){
 		if(s[i]!=" "){
 			q = q + 1 ;
 			if((s[i]==",")||(s[i]==".")){
 				q = q - 1 ;
 			}
+			c = q ;
 		}
 		else{
 			if(q>k){
@@ -16,7 +18,10 @@ function longestWordLen(s){
 			q = 0 ;
 		}
 	}
-	return k ;
+	if(k==0){
+		return c ;
+	}else{
+		return k ;
+	}
 }
-
 module.exports = longestWordLen ;	
