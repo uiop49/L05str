ced2ca03997a117e60930128065bf6be30207e56
MidnightYKT <nomiama_ykt@mail.ru>
2018-11-20 11:01:19 +0900

    01

---
+++ L05-01.js
@@ -0,0 +1,11 @@
+function numDots(s){
+    var res = 0;
+    for (var i = 0; i < s.length; i++)  {
+        if (s[i] == '.'){
+            res ++;
+        }
+    }
+    return res
+}
+
+module.exports = numDots
