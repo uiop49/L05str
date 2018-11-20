a10e71349b7ca9eab5028f5a10c9d0bb7679b88f
Ambros52 <vladapug52@gmail.com>
2018-11-20 09:43:28 +0900

    '2'

--- L05-02.js
+++ L05-02.js
@@ -1,7 +1,8 @@
 function ph2f(s) {
 	for (var i = 0; i < s.length; i++) {
-		if (s[i] == 'ph') {
+		if (s[i] == 'p' && s[i+1] == 'h') {
             s[i] = 'f';
+            s[i + 1] = '';
 		}
 	}
 	return s;
