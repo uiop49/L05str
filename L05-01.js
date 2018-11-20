1d57538da6e12999c9e761d2791e65cafe2cc816
petrovmm <petrovmm>
2018-11-20 11:23:41 +0900

    ''

--- L05-01.js
+++ L05-01.js
@@ -1,5 +1,12 @@
 function numDots(s) {
-	return 2;
+	s=String(s);
+    var k=0;
+    for(var i=0;i<s.length;i++){
+        if (s[i]=='.'){
+            k++;
+        }
+    }
+    return k;
 }
 
 module.exports = numDots;
