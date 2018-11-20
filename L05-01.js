c54982c5f68f5047668cfe3f7fe0acbd10ddf986
slava yaroev <slava@yaroev.com>
2018-11-20 11:32:52 +0900

    Begin

---
+++ L05-01.js
@@ -0,0 +1,10 @@
+function numDots(s){
+    var sdaasdsa = 0;
+    if (i=0;i<s.length;i++){
+        if (sdaasdsa = ".")
+        sdaasdsa++;
+    }
+    return sdaasdsa;
+}
+
+module.exports = numDots;
