f53a4e5428896452f229b10e2ecf82da37474ace
aino198 <ainomon1998q@gmail.com>
2018-11-20 09:47:47 +0900

    2 test needs to finish

--- L05-02.js
+++ L05-02.js
@@ -1,5 +1,14 @@
 function ph2s(s)
 {
-	return "";
+	var d="";
+	for (var i=0; i<s.length; i++)
+	{
+		if ((s[i] == "p") && (s[i+1] == "h"))
+		{
+			s[i]="f";
+			i++;
+		}
+	}
+	return s;
 }
 module.exports=ph2s;
