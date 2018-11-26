f6edabeff9accf0022d4d584a5c2b2c2835868f4
gavrilmikhailov <gavrilmikhailov@gmail.com>
2018-11-27 08:23:49 +0900

    лол

--- L05-05.js
+++ L05-05.js
@@ -2,7 +2,7 @@ function isDelResult(s1,s2) {
 	var sl1=s1.length;
 	var sl2=s2.length;
 	var l=0, i=0, j=0;
-	if (sl1<sl2)
+	// if (sl1<sl2)
 		for (i=0; i<sl1; i++)
 			for(j=0; j<sl2; j++)
 				if (s1[i]==s2[j])
