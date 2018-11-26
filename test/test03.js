d489b212d90eb3f048339712f4ee9cb30e4afa0b
NikolaevEM <43157756+NikolaevEM@users.noreply.github.com>
2018-11-27 03:38:35 +0900

    HotuAs testing

--- test/test03.js
+++ test/test03.js
@@ -1,1 +1,37 @@
+"use strict"                       
+
+const assert = require('assert');
+const fs = require('fs');
+const longestWordLen = require('../L05-03.js');
+
+describe('ЗАДАЧА 3: функция longestWordLen', function(){
+	it('файл L05-03.js существует ', function(){
+		assert(fs.existsSync('L05-03.js'), "Файл L05-03.js не существует");
+	});
+
+	it('файл непуст', function(){
+		var stats = fs.statSync('L05-03.js');
+		var fileSize = stats["size"];
+		assert(fileSize > 24, "Файл пуст или слишком маленький");
+	});
+
+	it('Правильно находит длину 1 слова', function(){
+		assert.strictEqual(longestWordLen("Quick"), 5, "неверно для слова 'Quick'");
+	});
+
+	it('Правильно находит макс длину слова в предложении1', function(){
+		assert.strictEqual(longestWordLen("Quicks brown fox jumps, and is gone."), 6,
+		 "неверно для предложения 'Quicks brown fox jumps, and is gone.'");
+	});
+
+	it('Правильно находит макс длину слова в предложении2', function(){
+		assert.strictEqual(longestWordLen("славянQuickess№ brown fox)Патриарх Кирилл, and)))is gone.."), 8,
+		 "неверно для предложения 'славянQuickess№ brown fox)Патриарх Кирилл, and)))is gone..'");
+	});
+
+	it('Правильно находит макс длину слова в пустом предложении', function(){
+		assert.strictEqual(longestWordLen(""), 0, "неверно для ''");
+	});
+
+});
  
