dd28bd364eb229583d092730b3398b108dd83d0c
gavrilmikhailov <gavrilmikhailov@gmail.com>
2018-11-27 09:10:19 +0900

    kjk

--- L05-05.js
+++ L05-05.js
@@ -2,13 +2,12 @@ function isDelResult(s1,s2) {
 	var sl1=s1.length;
 	var sl2=s2.length;
 	var l=0, i=0, j=0;
-	// if (sl1<sl2)
 		for (i=0; i<sl1; i++)
 			for(j=0; j<sl2; j++)
-				if (s1[i]==s2[j])
+				if (s1[i]==s2[j] || s2[i]==s1[j])
 					{l+=1; break;}
 	if (l==sl1 || l==sl2)
-		return 'true';
-	else return 'false';
+		return "true";
+	else return "false";
 }
 module.exports=isDelResult;
